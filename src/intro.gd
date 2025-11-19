0f6b772ab5f8d7301ceddb36dac69f09560f0322
Kwanghoon Son <kwangson@yahoo.com>
2025-11-19 19:21:26 +0900

    Add intro pic
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- src/intro.gd
+++ src/intro.gd
@@ -3,8 +3,13 @@ extends Node2D
 
 func _on_new_button_pressed() -> void:
 	SaveManager.remove_save_file()
-	_on_load_button_pressed()
+	$TextureRect.show()
+	$CanvasLayer/VBoxContainer.hide()
+	$Timer.start()
 
 func _on_load_button_pressed() -> void:
 	SaveManager.format_check()
 	get_tree().change_scene_to_file("res://src/game.tscn")
+
+func _on_timer_timeout() -> void:
+	_on_load_button_pressed()
